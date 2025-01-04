b62c0c2a1ea53fa447f325fed17677cb13dd6b00
bhupendra9211 <shahbhupendra9211@gmail.com>
2025-01-04 16:37:21 +0545

    playing with button

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,13 +3,16 @@ import { Button } from "@/components/ui/button"
 
 const Home = () => {
     return (
-        <main className='flex justify-center items-center h-full flex-col'>
+        <main className='flex justify-center items-center h-full flex-col gap-4'>
             <div>
                 Home
             </div>
             <button className="bg-purple-500 py-3 px-7 rounded my-4 hover:bg-purple-700"> click me </button>
             <Button>Click me</Button>
+            <Button className='bg-blue-700 font-bold' >Click me</Button>
             <Button variant="destructive">Destructive</Button>
+            <Button variant="destructive" size="lg" >Destructive</Button>
+            <Button variant="custom" >Custom</Button>
         </main>
     )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        custom: "bg-purple-500 py-3 px-7 rounded my-4 hover:bg-purple-700",
       },
       size: {
         default: "h-9 px-4 py-2",
