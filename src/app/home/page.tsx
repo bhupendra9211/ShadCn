8911fab2f4a952afa6e4b4cbc3cfe0499da03cc2
bhupendra9211 <shahbhupendra9211@gmail.com>
2025-01-04 15:41:56 +0545

    shad ch test

---
+++ src/app/home/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <main className='flex justify-center items-center h-full flex-col'>
+            <div>
+                Home
+            </div>
+            <button className="bg-purple-500 py-3 px-7 rounded my-4 hover:bg-purple-700"> click me </button>
+        </main>
+    )
+}
+
+export default Home
